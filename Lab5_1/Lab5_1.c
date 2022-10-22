fe4f2e2d556ba5fa4e59cee563c6b7d10f2ab2e4
TGPstudent <tarasenko.student@gmail.com>
2022-10-22 10:53:13 +0300

    Добавьте файлы проекта.

---
+++ Lab5_1/Lab5_1.c
@@ -0,0 +1,55 @@
+﻿// Lab5_1.c: Рiзниця елементiв одномiрних масивiв
+#define _CRT_SECURE_NO_WARNINGS
+#include <locale.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	setlocale(LC_CTYPE, "ukr");
+	const int N = 5;
+	typedef int Gtyp[5];
+	Gtyp a, b, c;
+	unsigned int i;
+	printf("\n Введiть значення елементiв масиву A: \n");
+	for (i = 0; i <= (N - 1); i++)
+	{
+		printf("\t а[%d]=", i + 1); scanf("%d", &a[i]);
+	}
+	printf("\n Введiть значення елементiв масиву B: \n");
+	for (i = 0; i <= (N - 1); i++)
+	{
+
+		printf("\t b[%d]=", i + 1); scanf("%d", &b[i]);
+		c[i] = a[i] - b[i];
+	}
+	system("cls");
+	printf("\n Ви ввели: Вектор A[1,%d] = {", N);
+	i = 0;
+	while (i <= N - 1)
+	{
+		printf("%d, ", a[i]);
+		i = i++;
+	}
+	printf("\b\b}");
+	printf("\n Ви ввели: Вектор B[1,%d] = {", N);
+	i = 0;
+	while (i <= N - 1)
+	{
+		printf("%d, ", b[i]);
+		i = i++;
+	}
+	printf("\b\b}\n");
+
+	printf("\n Результуючий вектор рiзницi: C[1,%d] = {", N);
+	i = 0;
+	while (i <= N - 1)
+	{
+		printf("%d, ", c[i]);
+		i = i++;
+	}
+	printf("\b\b}");
+	int getch(); getch();
+	return 0;
+}
